1923e814250eead69b8f759517406cbe055abca1
halltech-ci <odoo@eupd2.odoo.com>
2020-11-09 10:20:46 +0000

    adding sale order line

--- hta_mrp/__manifest__.py
+++ hta_mrp/__manifest__.py
@@ -16,17 +16,22 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Manufacturing/manufacturing',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base',
+               'sale_management',
+               'mrp',
+               ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sale_order_views.xml',
+        'views/mrp_production_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ hta_mrp/models/mrp_production.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+from odoo import models, fields, api
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+    
+    sale_order_id = fields.Many2one('sale.order', string="Commande", domain=[('state', '=', 'sale')])
+    description = fields.Text("Description")
+    mrp_order_line_ids = fields.One2many(related="sale_order_id.order_line", string="Order line", domain=[('in_mrp_line', '=', False)])
+    
+    @api.onchange('sale_order_id')
+    def _onchange_sale_order(self):
+        if not self.bom_id and self.sale_order_id.sale_mrp_product:
+            self.product_id = self.sale_order_id.sale_mrp_product
+            self.product_qty = 1.0
