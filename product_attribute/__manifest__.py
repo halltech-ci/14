bcdcf79b00697544926c4c2841dd1f2bff3c5359
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-17 23:58:06 +0000

    Adding security in product_attribute

--- product_attribute/__manifest__.py
+++ product_attribute/__manifest__.py
@@ -17,14 +17,14 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Product',
-    'version': '0.1',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['product'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
