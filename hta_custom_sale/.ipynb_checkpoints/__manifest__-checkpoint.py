52a770302fef1a9679127cc38244933850223284
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-18 01:54:15 +0000

    Adding sale secondary unit and conversion in hta_custom_sale

--- hta_custom_sale/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ hta_custom_sale/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_attribute",
+    'name': "hta_custom_sale",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,20 +16,19 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Product',
+    'category': 'Sale',
     'version': '14.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['product',
-               'stock',
-               ],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'views/product_views.xml',
+        'views/product_views.xml',
+        'views/sale_order_views.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
