1b835e6fe85c2b961a1cb584f18115cd1a633cc4
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-20 16:22:01 +0000

    adding description in maintenance_equipment

--- hta_project_custom/models/maintenance_equipment.py
+++ hta_project_custom/models/maintenance_equipment.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 
 class MaintenanceEquipement(models.Model):
     _name = "equipment.equipment"
-    _rescription = "My custom equipement"
+    _description = "My custom equipement"
     
     project_task_id = fields.Many2one('project.task', string="Task", ondelete="cascade")
     equipment_id = fields.Many2one('maintenance.equipment')
