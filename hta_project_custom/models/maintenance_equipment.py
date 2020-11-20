1e8ea1ed92e0e2d29452df6c6c004b465aaccc02
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-20 14:53:50 +0000

    Adding equipment in project task

--- hta_project_custom/models/maintenance_equipment.py
+++ hta_project_custom/models/maintenance_equipment.py
@@ -4,6 +4,9 @@ from odoo import models, fields, api
 
 
 class MaintenanceEquipement(models.Model):
-    _inherit = "maintenance.equipment"
+    _name = "equipment.equipment"
+    _rescription = "My custom equipement"
     
     project_task_id = fields.Many2one('project.task', string="Task", ondelete="cascade")
+    equipment_id = fields.Many2one('maintenance.equipment')
+    equipment_category = fields.Many2one(related="equipment_id.category_id", string="Category")
