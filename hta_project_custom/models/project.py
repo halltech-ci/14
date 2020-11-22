c6f23ec8b8217c2994d632b299b4942c411642c8
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-22 02:07:21 +0000

    Adding stock move in project task

--- hta_project_custom/models/project.py
+++ hta_project_custom/models/project.py
@@ -3,40 +3,18 @@
 from odoo import models, fields, api
 
 
-class ProjectTask(models.Model):
-    _inherit = "project.task"
-    
-    material_ids = fields.One2many(
-        comodel_name="project.task.material",
-        inverse_name="task_id",
-        string="Product Used",
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+
+    location_source_id = fields.Many2one(
+        comodel_name='stock.location',
+        string='Source Location',
+        index=True,
+        help='Default location from which materials are consumed.',
     )
-    
-    equipment_ids = fields.One2many('equipment.equipment', "project_task_id")
-
-
-class ProjectTaskMaterial(models.Model):
-    """Added Product and Quantity in the Task Material Used."""
-
-    _name = "project.task.material"
-    _description = "Task Material Used"
-
-    task_id = fields.Many2one(
-        comodel_name="project.task", string="Task", ondelete="cascade", required=True
+    location_dest_id = fields.Many2one(
+        comodel_name='stock.location',
+        string='Destination Location',
+        index=True,
+        help='Default location to which materials are consumed.',
     )
-    product_id = fields.Many2one(
-        comodel_name="product.product", string="Product", required=True
-    )
-    quantity = fields.Float(string="Quantity")
-    initial_qty = fields.Float(string="Initial Qty")
-    qty_used = fields.Float(string="Quantity Used")
-    product_code = fields.Char(related="product_id.default_code")
-
-    @api.constrains("quantity")
-    def _check_quantity(self):
-        for material in self:
-            if not material.quantity > 0.0:
-                raise ValidationError(
-                    _("Quantity of material consumed must be greater than 0.")
-                )
-                
