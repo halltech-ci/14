6db929287d3c8745d849aa06ebfa4315dae44109
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-22 02:07:21 +0000

    Adding stock move in project task

---
+++ hta_project_custom/models/stock_move.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+    
+    task_product_id = fields.One2many(
+        'project.task.material',
+        'stock_move_id',
+        string='Project Task Product',
+    )
+
+    @api.model
+    def _action_done(self):
+        # The analytical amount is updated with the value of the
+        # stock movement, because if the product has a tracking by
+        # lot / serial number, the cost when creating the
+        # analytical line is not correct.
+        res = super(StockMove, self)._action_done()
+        self.mapped('task_product_id')._update_unit_amount()
+        return res
