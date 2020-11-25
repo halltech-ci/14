59537ee03beddedc3fdf8505f8cc01d7b310e3c8
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-25 14:24:52 +0000

    Adding project_stock_request

---
+++ project_stock_request/models/project_task.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    stock_request_order_ids = fields.One2many('stock.request.order',
+                                              'project_task_id',
+                                              string='Stock Request Orders')
