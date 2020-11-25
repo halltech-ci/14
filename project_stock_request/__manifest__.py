4a61b38512a4a5523ca6ea8c3b52375ebdccd626
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-25 14:47:04 +0000

    Adding project_stock_request

--- project_stock_request/__manifest__.py
+++ project_stock_request/__manifest__.py
@@ -16,11 +16,13 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Project',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['project';
+               'warehouse',
+               ],
 
     # always loaded
     'data': [
