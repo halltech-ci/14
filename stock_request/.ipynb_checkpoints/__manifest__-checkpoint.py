46d181163261ed4bdeeb89d167863076cec84eb3
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-26 08:17:56 +0000

    Adding separate stock_request

--- stock_request/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ stock_request/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "project_stock_request",
+    'name': "stock_request",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,17 +16,15 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Project',
+    'category': 'Warehouse',
     'version': '14.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['project',
-               ],
+    'depends': ['stock_request_order'],
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
-        #'security/stock_request_security.xml',
+        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
