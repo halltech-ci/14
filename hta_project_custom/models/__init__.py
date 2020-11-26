9c362c5dc3e228684b7c190791d8766b0c8e0c39
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-26 00:08:32 +0000

    deleting hta_project_custom

--- hta_project_custom/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#from . import models
-from . import maintenance_equipment
-#from . import project_task_material
-#from . import project_task
-#from . import stock_move
-#from . import project
