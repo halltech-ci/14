3ea0a7ba8aefb24cacc0f0f6b73be7e9a13c16ce
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-28 22:07:00 +0000

    Adding project stock move

---
+++ project_stock_move/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+#from . import models
+from . import project
+from . import project_task
+from . import project_stock_request
--- project_stock_request/models/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import project
-from . import project_task
-from . import stock_request_order
