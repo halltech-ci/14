5969508faf6b70c0a99213f922d338328d37b593
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-28 22:38:40 +0000

    Adding project stock move

---
+++ project_stock_request/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#from . import models
+from . import project_stock_request
+from . import res_config_settings
+from . import res_company
+from . import stock_request_allocation
+from . import stock_request_abstract
+from . import stock_request
+from . import stock_warehouse
