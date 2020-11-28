b5692d9c2d93cb9ec2f962c8a53b15e178bf1816
halltech-ci <maurice.atche@halltech-africa.com>
2020-11-28 09:18:16 +0000

    Adding stock_request and project_stock_request

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -25,8 +25,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/stock_request_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
