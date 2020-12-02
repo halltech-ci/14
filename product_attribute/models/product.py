e9c69053321832dfe5948af44f6a1cedc9a8c7a9
halltech-ci <maurice.atche@halltech-africa.com>
2020-12-02 22:13:31 +0000

    Adding product code unique in product_attribute

--- product_attribute/models/product.py
+++ product_attribute/models/product.py
@@ -5,7 +5,14 @@ from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = "product.product"
     
-_sql_constraints = [
+    def _get_default_code(self):
+        return self.env["ir.sequence"].next_by_code("product.default.code")
+
+    default_code = fields.Char(
+        "Internal Reference", index=True, default=_get_default_code
+    )
+    
+    _sql_constraints = [
         (
             "default_code_uniq",
             "unique(default_code)",
