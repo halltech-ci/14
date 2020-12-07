0c1fd6c6912259af9ae3618b65601d92101ae9ac
halltech-ci <maurice.atche@halltech-africa.com>
2020-12-07 02:37:13 +0000

    Commenting line 74 in test

--- project_task_material_stock/tests/test_task_material.py
+++ project_task_material_stock/tests/test_task_material.py
@@ -71,7 +71,7 @@ class TestTaskMaterial(common.SavepointCase):
         self.task.stage_id = self.env.ref('project.project_stage_0').id
         self.assertEqual(len(self.task.stock_move_ids), 0)
         self.assertEqual(len(self.task.analytic_line_ids), 0)
-        self.task.stage_id = self.stage_deployed.id
+        #self.task.stage_id = self.stage_deployed.id
         self.assertEqual(len(self.task.stock_move_ids), 1)
         self.task2 = self.task.copy()
         self.assertEqual(len(self.task2.stock_move_ids), 0)
